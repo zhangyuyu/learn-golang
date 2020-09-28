7f70112ef3a800c6e1006f9329dee1c0fc82bf85
zhangyuyu <1580074674@qq.com>
2020-09-28 19:45:32 +0800

    [Yukki] Go by Example - function

---
+++ feature/12-function.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	res := plus(1, 2)
+	fmt.Println("1 + 2 = ", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1 + 2 + 3 = ", res)
+}
+
+func plusPlus(a int, b int, c int) int {
+	return a + b + c
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
