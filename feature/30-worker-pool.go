4e4ef311f4fea203f8c13c7297ca607be41a0728
zhangyuyu <1580074674@qq.com>
2020-09-29 20:18:32 +0800

    [Yukki] Go by Example - worker pool

---
+++ feature/30-worker-pool.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	t1 := time.Now()
+
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		fmt.Println("result: ", <-results)
+	}
+
+	fmt.Println("Total Time", time.Since(t1))
+}
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
