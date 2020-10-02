c3fe2cd9e2147f8c441454d42ff8810b4c7e9c1e
zhangyuyu <1580074674@qq.com>
2020-10-02 16:22:11 +0800

    [Yukki] Go by Example - line filter

---
+++ feature/54-line-filter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(ucl)
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(0)
+	}
+}
+
+//  cat defer.txt| go run feature/54-line-filter.go
