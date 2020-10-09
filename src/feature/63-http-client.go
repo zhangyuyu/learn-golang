5fda91de1ddb096065c0a39ffaf817007b480321
zhangyuyu <1580074674@qq.com>
2020-10-09 10:05:21 +0800

    [Yukki] Go by Example - http client.

---
+++ src/feature/63-http-client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+
+	"GolangHelloworld/src/utils"
+)
+
+func main() {
+	resp, err := http.Get("http://gobyexample.com")
+	utils.Check(err)
+
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status: ", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+}
