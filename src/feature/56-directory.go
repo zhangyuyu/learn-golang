e58059924355daa439c751b9de0ce21aab93e5cf
zhangyuyu <1580074674@qq.com>
2020-10-09 08:59:24 +0800

    [Yukki] Go by Example - directory

---
+++ src/feature/56-directory.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"GolangHelloworld/src/utils"
+)
+
+func main() {
+	tmpdir := "tmpdir"
+	err := os.Mkdir(tmpdir, 0755)
+	utils.Check(err)
+
+	defer os.RemoveAll(tmpdir)
+
+	createEmptyFile := func(name string) {
+		data := []byte("")
+		utils.Check(ioutil.WriteFile(name, data, 0644))
+	}
+
+	createEmptyFile(tmpdir + "/file1")
+	err = os.MkdirAll(tmpdir+"/parent/child", 0755)
+	utils.Check(err)
+
+	createEmptyFile(tmpdir + "/parent/file2")
+	createEmptyFile(tmpdir + "/parent/file3")
+	createEmptyFile(tmpdir + "/parent/child/file4")
+
+	dir, err := ioutil.ReadDir(tmpdir + "/parent")
+	utils.Check(err)
+
+	fmt.Println("Listing tmpdir/parent")
+	for _, entry := range dir {
+		fmt.Println(" |-", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir(tmpdir + "/parent/child")
+	utils.Check(err)
+
+	dir, err = ioutil.ReadDir(".")
+	utils.Check(err)
+
+	fmt.Println("Listing tmpdir/parent/child")
+	for _, entry := range dir {
+		fmt.Println(" |-", entry.Name(), entry.IsDir())
+	}
+
+	err = os.Chdir("../../..")
+	utils.Check(err)
+
+	fmt.Println("Visiting tmpdir")
+	err = filepath.Walk("tmpdir", visit)
+
+}
+
+func visit(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Println(" |-", path, info.IsDir())
+	return nil
+}
