a9ac29f1f6c4ecbe56edc9006720a9e16b4cfc39
zhangyuyu <1580074674@qq.com>
2020-10-09 09:08:15 +0800

    [Yukki] Go by Example - temporary files and directories.

---
+++ src/feature/57-temporary-files-and-directories.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"GolangHelloworld/src/utils"
+)
+
+func main() {
+	file, err := ioutil.TempFile("", "sample")
+	utils.Check(err)
+	fmt.Println("Temp file name: ", file.Name())
+
+	defer os.Remove(file.Name())
+
+	_, err = file.Write([]byte{1, 2, 3, 4})
+	utils.Check(err)
+
+	dir, err := ioutil.TempDir("", "sampledir")
+	utils.Check(err)
+	fmt.Println("Temp dir name: ", dir)
+
+	defer os.RemoveAll(dir)
+
+	fname := filepath.Join(dir, "file1")
+	err = ioutil.WriteFile(fname, []byte{1, 2}, 0666)
+	utils.Check(err)
+}
+
+// Temp file name:  /var/folders/t7/zh_h0h0d5vn0c2xsr_1tfqnh0000gn/T/sample018699025
+// Temp dir name:  /var/folders/t7/zh_h0h0d5vn0c2xsr_1tfqnh0000gn/T/sampledir214998844
