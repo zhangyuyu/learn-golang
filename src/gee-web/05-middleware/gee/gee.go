67cc90c2749b083e05611671bfcfea704656208a
zhangyuyu <1580074674@qq.com>
2020-10-10 12:46:55 +0800

    [Yukki] gee web - 中间件Middleware

---
+++ src/gee-web/05-middleware/gee/gee.go
@@ -0,0 +1,74 @@
+package gee
+
+import (
+	"net/http"
+	"strings"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	*RouterGroup
+	router *router
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (group *RouterGroup) addRoute(method string, pattern string, handler HandlerFunc) {
+	group.engine.router.addRoute(method, group.prefix+pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(request.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+
+	context := newContext(writer, request)
+	context.handlers = middlewares
+	engine.router.handle(context)
+}
--- src/gee-web/README.md
+++ src/gee-web/README.md
@@ -5,7 +5,7 @@
 2. [第二天：上下文设计(Context)](https://geektutu.com/post/gee-day2.html)
 3. [第三天：Trie树路由(Router)](https://geektutu.com/post/gee-day3.html)
 4. [第四天：分组控制(Group)](https://geektutu.com/post/gee-day4.html)
-5. [第五天：中间件(Middleware)]()
+5. [第五天：中间件(Middleware)](https://geektutu.com/post/gee-day5.html)
 6. [第六天：HTML模板(Template)]()
 7. [第七天：错误恢复(Panic Recover)]()
 
