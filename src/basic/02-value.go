7541765676c4226fc0be2bb8708cf3019dd6438b
yukkizhang <yukkizhang@tencent.com>
2021-12-08 20:18:18 +0800

    [Yukki] go embed

--- src/basic/02-value.go
+++ src/basic/02-value.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 )
 
+//go:embed 00-README.md
+var some string
+
 func main() {
+	fmt.Println(some)
 	fmt.Println("1 + 1 = ", 1+1)
 	fmt.Println("7.0/3.0 = ", 7.0/3.0)
 	fmt.Println(!true)
